4eb2dc253b891f1391f4401bbc481300cd5a6150
Tom LaTulipe <tlatulipe@gmail.com>
2022-04-02 17:55:45 -0400

    Good heavens, please remember to put defer

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+
+let form = document.querySelector("#inputForm");
+console.log(form); //null? FORGOT DEFER
+const error = document.querySelector("#errormsg");
+console.log(error); //null? FORGOT DEFER
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); //prevent page from refreshing
+    let text = event.target.todoField.value; //Submit button is linked to 
+                                            //the textfield (id todoField), and that's where 
+                                            //this is pulled from
+    console.log(text);
+});
